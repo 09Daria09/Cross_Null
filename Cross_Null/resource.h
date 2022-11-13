2f068ca1a34f8464288d639b9fe7f943b503f798
Daria <kukuruzadasha86@gmail.com>
2022-11-13 19:48:26 +0200

    Add project files.

---
+++ Cross_Null/resource.h
@@ -0,0 +1,29 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Cross_Null.rc
+//
+#define IDD_DIALOG1                     101
+#define IDB_BITMAP1                     102
+#define IDB_BITMAP2                     103
+#define IDC_BUTTON16                    1001
+#define IDC_BUTTON2                     1002
+#define IDC_BUTTON17                    1002
+#define IDC_BUTTON19                    1003
+#define IDC_BUTTON15                    1004
+#define IDC_BUTTON18                    1005
+#define IDC_BUTTON20                    1006
+#define IDC_BUTTON21                    1007
+#define IDC_BUTTON22                    1008
+#define IDC_BUTTON23                    1009
+#define IDC_BUTTON10                    1010
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
